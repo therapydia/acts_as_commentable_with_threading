7d627d905e79eba9178577f942063c236bc0d47a
Eric Steen <rubycoder1@gmail.com>
2013-07-08 11:49:45 -0700

    Update acts_as_commentable_with_threading.rb

--- lib/acts_as_commentable_with_threading.rb
+++ lib/acts_as_commentable_with_threading.rb
@@ -16,6 +16,7 @@ module Acts #:nodoc:
 
   module ClassMethods
     def acts_as_commentable
+      has_many :comments, :class_name => "Comment", :as => :commentable
       has_many :comment_threads, :class_name => "Comment", :as => :commentable
       before_destroy { |record| record.root_comments.destroy_all }
       include Acts::CommentableWithThreading::LocalInstanceMethods
